337d05ae62ff07162326fc3c0dd18a549cae91ff
Radheshyam1234 <88738458+Radheshyam1234@users.noreply.github.com>
2021-08-17 07:22:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./styles.css";
+var emojiDictionary = {
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": " Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😉": "Winking Face",
+  "❤️": " Red Heart",
+  "😕": " Confused Face",
+  "😠": " Angry Face",
+  "😪": "Sleepy Face"
+};
+
+var knownEmoji = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  var [input, setInput] = useState("");
+
+  function knowName(emoji) {
+    var meaning = emojiDictionary[emoji];
+
+    if (meaning === undefined) {
+      setMeaning("Emoji not present");
+    } else {
+      setMeaning(meaning);
+      setInput(emoji);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Interpretor</h1>
+
+      <input
+        style={{
+          fontSize: "1.5rem",
+          padding: "1rem",
+          width: "60%",
+          border: "2px solid red"
+        }}
+        value={input}
+        onChange={(e) => {
+          knowName(e.target.value);
+          setInput(e.target.value);
+        }}
+      />
+      <h2 style={{ color: "blueviolet" }}>{meaning} </h2>
+
+      <div>
+        <h2>We have following emoji in our database</h2>
+        {knownEmoji.map((emoji, index) => {
+          return (
+            <span
+              key={index}
+              style={{ cursor: "pointer", fontSize: "1.5rem", padding: "5px" }}
+              onClick={() => {
+                knowName(emoji);
+              }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
